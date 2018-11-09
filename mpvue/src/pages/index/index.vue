c7087e17d3e907f9d8c349e9ddd1ba5c6258f3d4
MengXiTong <1317545539@qq.com>
2018-11-09 18:34:07 +0800

    echarts测试

--- mpvue/src/pages/index/index.vue
+++ mpvue/src/pages/index/index.vue
@@ -19,51 +19,131 @@
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
     </form>
     <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+
+    <div class="echarts-wrap">
+      <mpvue-echarts :echarts="echarts" :onInit="initChart" />
+    </div>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
+import card from "@/components/card";
+import mpvueEcharts from "mpvue-echarts";
+import * as echarts from "echarts";
 
 export default {
-  data () {
+  data() {
     return {
-      motto: 'Hello World',
-      userInfo: {}
-    }
+      motto: "Hello World",
+      userInfo: {},
+      echarts,
+      resdata: []
+    };
   },
 
   components: {
-    card
+    card,
+    mpvueEcharts
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
+    bindViewTap() {
+      const url = "../logs/main";
+      wx.navigateTo({ url });
     },
-    getUserInfo () {
+    getUserInfo() {
       // 调用登录接口
       wx.login({
         success: () => {
           wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
+            success: res => {
+              this.userInfo = res.userInfo;
             }
-          })
+          });
         }
-      })
+      });
     },
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
+    clickHandle(msg, ev) {
+      console.log("clickHandle:", msg, ev);
+    },
+    // 这里只是做一个测试，方法里可以调用接口，获取到的数据赋值给resdata
+    test() {
+      const res = [8000, 12000, 10000, 16000, 14000, 18000, 16000];
+      this.resdata = res;
+    },
+    initChart(canvas, width, height) {
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+      canvas.setChart(chart);
+      var option = {
+        backgroundColor: "#fff",
+        color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
+        tooltip: {
+          trigger: "axis"
+        },
+        legend: {
+          data: []
+        },
+        grid: {
+          containLabel: true
+        },
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          axisLine: {
+            lineStyle: {
+              color: "#ccc"
+            }
+          },
+          data: ["0:00", "4:00", "8:00", "12:00", "16:00", "20:00", "24:00"]
+        },
+        yAxis: {
+          x: "center",
+          type: "value",
+          min: 8000, // 坐标轴最小值
+          max: 20000,
+          scale: true,
+          // 坐标轴的颜色
+          axisLine: {
+            lineStyle: {
+              color: "#ccc"
+            }
+          }
+        },
+        series: [
+          {
+            name: "",
+            type: "line",
+            smooth: false, // true是曲线，false是直线
+            data: this.resdata,
+            // 设置折线区域颜色
+            areaStyle: { normal: {} },
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  { offset: 0, color: "skyblue" }
+                ])
+              }
+            }
+          }
+        ]
+      };
+      chart.setOption(option);
+      return chart;
     }
   },
 
-  created () {
+  created() {
     // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
+    this.getUserInfo();
+  },
+
+  onShow() {
+    this.test();
   }
-}
+};
 </script>
 
 <style scoped>
@@ -101,5 +181,9 @@ export default {
   padding: 5px 10px;
   color: blue;
   border: 1px solid blue;
+}
+.echarts-wrap {
+  width: 100%;
+  height: 300px;
 }
 </style>
